a264642c915ace6f03793fcbad7e57c4a828b5d1
SamuelVersiane <samuellversiane@gmail.com>
2021-11-16 21:08:40 -0300

    Instantiation of database

---
+++ workshopmongo/src/main/java/com/sam/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.sam.workshopmongo.config;
+
+import com.sam.workshopmongo.domain.User;
+import com.sam.workshopmongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        userRepository.saveAll(
+                Arrays.asList(new User(null, "Maria Brow", "maria@email.com"),
+                        new User(null, "ALex Green", "alex@email.com"),
+                        new User(null, "Bob Grey", "bob@email.com")));
+    }
+}
+
+
+
